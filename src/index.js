8485c728c0fce570c23e9f0278cefdd096a6ed6c
Osaukhumwen Iyamuosa <osaukhu.bi@gmail.com>
2019-10-04 11:42:04 +0100

    chore(server):setup node/express server with mongodb connection
    [Finishes #168948653]

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import express, { urlencoded, json } from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import debug from 'debug';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const DB_URL = process.env.NODE_ENV === 'development' ? process.env.MONGODB_URI : process.env.MONGODB_URI_TEST;
+const log = debug('dev');
+
+// Express Middlewares
+app.use(urlencoded({ extended: true }));
+app.use(json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+});
+
+const db = mongoose.connection;
+
+db.on('error', (err) => log(err));
+
+db.once('open', () => {
+  log(`Server started on port ${PORT} and database connection successful`);
+});
